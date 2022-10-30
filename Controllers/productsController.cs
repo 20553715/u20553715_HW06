53d9f2b00529051f2107a890ad190c4a57644afc
20553715 <u20553715@tuks.co.za>
2022-10-30 22:50:20 +0200

    Add project files.

---
+++ Controllers/productsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
+using Homework06.Models;
+
+namespace Homework06.Controllers
+{
+    public class productsController : Controller
+    {
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        public JsonResult BarChartDataEF()
+        {
+            var data = db.products.ToList();
+            Chart _chart = new Chart();
+            _chart.labels = data.Select(x => x.category_id == 6 && x.order.order_date.Month == 1).ToList();
+            _chart.datasets = new List<Datasets>();
+            List<Datasets> _dataSet = new List<Datasets>();
+
+           /* int[] ChartData = new int[]
+            {
+                db.order_items.Where(x => x.product.category_id == 6 && x.order.order_date.Month == 1).ToList().Sum(x => x.quantity)
+            };*/
+            _dataSet.Add(new Datasets()
+            {
+                label = "Current Year",
+                data = data.Select(x => x.model_year.).ToArray(),
+                backgroundColor = new string[] { "#FFFFFF", "#000000", "#FF00000" },
+                borderColor = new string[] { "#FFFFFF", "#000000", "#FF00000" },
+                borderWidth = "1"
+
+            });
+            _chart.datasets = _dataSet;
+            return Json(_chart, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: products
+        public ActionResult Index(string search, int? i)
+        {
+            //var products = db.products.Include(p => p.brand).Include(p => p.category);
+            List<product> lprod = db.products.ToList();
+            return View(db.products.Where(x => x.product_name.StartsWith(search) || search == null).ToList().ToPagedList(1, 10));
+            //return View(products.ToList().ToPagedList(1,10));
+        }
+
+        // GET: products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: products/Create
+        public ActionResult Create()
+        {
+            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name");
+            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name");
+            return View();
+        }
+
+        // POST: products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+            return View(product);
+        }
+
+        // GET: products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+            return View(product);
+        }
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+            return View(product);
+        }
+
+        // GET: products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        //public JsonResult Delete(int ID)
+        //{
+        //    var data = _context.User.FirstOrDefault(x => x.Id == ID);
+        //    _context.User.Remove(data);
+        //    _context.SaveChanges();
+        //    return Json(JsonRequestBehavior.AllowGet);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
